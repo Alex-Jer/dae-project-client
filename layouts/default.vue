ce67f30d1e7b73dcf314530a0638cffd5362bbd3
Alexandre <alexandrejeronimo2001@gmail.com>
2023-01-10 20:10:38 +0000

    layout fix

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,60 +1,46 @@
 <template>
-  <div>
-    <nav
-      class="navbar header has-shadow is-primary"
-      role="navigation"
-      aria-label="main navigation"
-    >
-      <div class="navbar-brand">
-        <a class="navbar-item" href="/">
-          <img src="~assets/buefy.png" alt="Buefy" height="28" />
-        </a>
-
-        <div class="navbar-burger">
-          <span />
-          <span />
-          <span />
-        </div>
-      </div>
-    </nav>
-
-    <section class="main-content columns">
-      <aside class="column is-2 section">
-        <p class="menu-label is-hidden-touch">General</p>
-        <ul class="menu-list">
-          <li v-for="(item, key) of items" :key="key">
-            <NuxtLink :to="item.to" exact-active-class="is-active">
-              <b-icon :icon="item.icon" /> {{ item.title }}
-            </NuxtLink>
-          </li>
-        </ul>
-      </aside>
-
-      <div class="container column is-10">
-        <Nuxt />
-      </div>
-    </section>
+  <div id="app">
+    <nav-bar />
+    <aside-menu :menu="menu" />
+    <nuxt></nuxt>
   </div>
 </template>
 
+<!-- <template>
+  <div>
+    <nav-bar />
+    <aside-menu :menu="menu" />
+    <Nuxt />
+  </div>
+</template> -->
+
 <script>
-export default {
-  name: 'DefaultLayout',
+import { defineComponent } from 'vue'
+import menu from '@/assets/js/menu.js'
+import NavBar from '@/components/NavBar.vue'
+import AsideMenu from '@/components/AsideMenu.vue'
+
+export default defineComponent({
+  name: 'AppComponent',
+  components: {
+    AsideMenu,
+    NavBar,
+  },
   data() {
     return {
-      items: [
-        {
-          title: 'Home',
-          icon: 'home',
-          to: { name: 'index' },
-        },
-        {
-          title: 'Inspire',
-          icon: 'lightbulb',
-          to: { name: 'inspire' },
-        },
-      ],
+      menu,
     }
   },
-}
+  mounted() {
+    this.$store.dispatch('toggleFullPage', false)
+    this.$store.dispatch('asideDesktopOnlyToggle', true)
+
+    this.$store.commit('user', {
+      name: this.$store.state.userName,
+      email: this.$store.state.email,
+      avatar: 'https://avatars.dicebear.com/v2/gridy/John-Doe.svg',
+    })
+  },
+  created() {},
+})
 </script>
