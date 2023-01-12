e1464464e8efe5b7f3d37ce17c54e0b3e6c4d1d6
tgn <trwygon@outlook.pt>
2023-01-12 18:07:15 +0000

    Finished Policy Details

--- pages/policies/_id/index.vue
+++ pages/policies/_id/index.vue
@@ -5,6 +5,12 @@
       <span class="subtitle">#{{ id }}</span>
     </hero-bar>
     <section class="section is-main-section">
+      <card-component title="Details" icon="ballot">
+        <b-field label="Policy Code" horizontal> {{ id }} </b-field>
+        <b-field label="Insurer Company" horizontal> {{ policy.insurerCompany }} </b-field>
+        <b-field label="Type" horizontal> {{ policy.type }} </b-field>
+        <b-field label="Covers" horizontal> {{ formatCovers ?? 'unknown' }} </b-field>
+      </card-component>
       <card-component class="has-table has-mobile-sort-spaced" title="Occurrences" icon="clipboard-list">
         <occurrences-table :occurrences="occurrences" :show-policy="false" />
       </card-component>
@@ -29,14 +35,28 @@ export default defineComponent({
   data() {
     return {
       occurrences: [],
+      policy: {},
     }
   },
   computed: {
     id() {
       return this.$route.params.id
     },
+    formatCovers() {
+      if (this.policy.covers) {
+        let covers = ''
+        this.policy.covers.forEach((cover) => {
+          covers += cover + ', '
+        })
+        return covers.slice(0, -2)
+      }
+      return ''
+    },
   },
   created() {
+    this.$axios.$get(`/api/policies/${this.id}`).then((policy) => {
+      if (policy && policy[0]) this.policy = policy[0]
+    })
     this.$axios.$get(`/api/policies/${this.id}/occurrences`).then((occurrences) => {
       this.occurrences = occurrences
     })
