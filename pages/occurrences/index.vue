b8deddf6b98fa6472cd762891bce27958ee12150
tgn <trwygon@outlook.pt>
2023-01-12 23:18:56 +0000

    Dynamic Occurences Page
    Changed by user type

--- pages/occurrences/index.vue
+++ pages/occurrences/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <hero-bar>
-      My Occurrences
+      {{ getName }}
       <router-link slot="right" to="occurrences/create" class="button is-info">
         <b-icon icon="plus-circle" custom-size="default" />
         <span>New Occurrence</span>
@@ -34,8 +34,35 @@ export default defineComponent({
       occurrences: [],
     }
   },
+  computed: {
+    getName() {
+      switch (this.$auth.user.type) {
+        case 'Customer':
+          return `My Occurrences`
+        case 'Repairer':
+          return `Approved Occurrences`
+        case 'Expert':
+          return `Pending Occurrencces`
+        default:
+          return `All Occurrences`
+      }
+    },
+
+    getUrl() {
+      switch (this.$auth.user.type) {
+        case 'Customer':
+          return `/api/customers/${this.$auth.user.vat}/occurrences`
+        case 'Repairer':
+          return `/api/occurrences/approved`
+        case 'Expert':
+          return `/api/occurrences/pending`
+        default:
+          return `/api/occurrences`
+      }
+    },
+  },
   created() {
-    this.$axios.$get(`/api/customers/${this.$auth.user.vat}/occurrences`).then((occurrences) => {
+    this.$axios.$get(this.getUrl).then((occurrences) => {
       this.occurrences = occurrences
     })
   },
