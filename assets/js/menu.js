3cc76a9580d705153c661c0fb152510beb9c60df
tgn <trwygon@outlook.pt>
2023-01-13 01:11:27 +0000

    Update menu.js

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -20,21 +20,6 @@ export default [
       icon: 'table',
     },
     {
-      to: '/forms',
-      label: 'Forms',
-      icon: 'square-edit-outline',
-    },
-    {
-      to: '/profile',
-      label: 'Profile',
-      icon: 'account-circle',
-    },
-    {
-      to: '/login',
-      label: 'Login',
-      icon: 'lock',
-    },
-    {
       label: 'Submenus',
       subLabel: 'Submenus Example',
       icon: 'view-list',
